7e226f13998cece5c2e51457f75e2aad14cb4db8
Oleksandr Prylutskyy <sashaprylutskyy@gmail.com>
2025-10-23 22:26:15 +0300

    Relocated login and register endpoints to AuthController.java

---
+++ src/main/java/com/sashaprylutskyy/squidgamems/controller/AuthController.java
@@ -0,0 +1,37 @@
+package com.sashaprylutskyy.squidgamems.controller;
+
+import com.sashaprylutskyy.squidgamems.model.dto.UserRequestDTO;
+import com.sashaprylutskyy.squidgamems.model.dto.UserResponseDTO;
+import com.sashaprylutskyy.squidgamems.model.interfaceGroup.OnCreate;
+import com.sashaprylutskyy.squidgamems.model.interfaceGroup.OnLogin;
+import com.sashaprylutskyy.squidgamems.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final UserService userService;
+
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<UserResponseDTO> createUser(@Validated(OnCreate.class) @RequestBody UserRequestDTO dto) {
+        UserResponseDTO userResponse = userService.registerUser(dto);
+        return new ResponseEntity<>(userResponse, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@Validated(OnLogin.class) @RequestBody UserRequestDTO dto) {
+        String jwt = userService.login(dto);
+        return ResponseEntity.ok(jwt);
+    }
+}
--- src/main/java/com/sashaprylutskyy/squidgamems/controller/UserController.java
+++ src/main/java/com/sashaprylutskyy/squidgamems/controller/UserController.java
@@ -1,38 +1,8 @@
 package com.sashaprylutskyy.squidgamems.controller;
 
-import com.sashaprylutskyy.squidgamems.model.dto.UserRequestDTO;
-import com.sashaprylutskyy.squidgamems.model.dto.UserResponseDTO;
-import com.sashaprylutskyy.squidgamems.model.interfaceGroup.OnCreate;
-import com.sashaprylutskyy.squidgamems.model.interfaceGroup.OnLogin;
-import com.sashaprylutskyy.squidgamems.service.UserService;
-import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
 public class UserController {
 
-    private final UserService userService;
-
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
-
-    @PostMapping("/auth/register")
-    public ResponseEntity<UserResponseDTO> createUser(@Validated(OnCreate.class) @RequestBody UserRequestDTO dto) {
-        UserResponseDTO userResponse = userService.registerUser(dto);
-        return new ResponseEntity<>(userResponse, HttpStatus.CREATED);
-    }
-
-    @PostMapping("/auth/login")
-    public ResponseEntity<String> login(@Validated(OnLogin.class) @RequestBody UserRequestDTO dto) {
-        String jwt = userService.login(dto);
-        return ResponseEntity.ok(jwt);
-    }
 }
