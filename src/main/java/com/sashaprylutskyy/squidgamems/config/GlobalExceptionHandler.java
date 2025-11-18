d38550aaf7644bd732fd8aed126324aa2888c2be
Oleksandr Prylutskyy <sashaprylutskyy@gmail.com>
2025-11-18 14:08:23 +0200

    Changes exception handling logic from "unauthenticated" to "bad request"

--- src/main/java/com/sashaprylutskyy/squidgamems/config/GlobalExceptionHandler.java
+++ src/main/java/com/sashaprylutskyy/squidgamems/config/GlobalExceptionHandler.java
@@ -77,6 +77,6 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<Map<String, String>> handleRuntimeException(RuntimeException e) {
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(Map.of("error", e.getMessage()));
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of("error", e.getMessage()));
     }
 }
